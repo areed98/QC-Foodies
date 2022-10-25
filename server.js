d2804dd868660fb7f521899fd4d515e31393c62e
Benny <benny.le890@gmail.com>
2022-10-25 18:43:41 -0400

    fixed photo for handlebars

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 
 app.use(require('./controllers/'));
 
